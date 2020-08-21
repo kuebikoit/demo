b3b4b876b636126e7278cd2d581f682142e7953f
Karya Chhetri <karyachhetri@karya.local>
2020-08-21 08:31:36 -0700

    log level

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.SQSService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
@@ -21,7 +23,7 @@ public class MessageController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String sendMessage(@RequestBody String message) {
-        System.out.println("Message is " + message);
+        log.info("Message is {}", message);
         service.sendMessage(message);
 
         return "SUCCESS";
--- src/main/java/com/example/demo/service/SQSService.java
+++ src/main/java/com/example/demo/service/SQSService.java
@@ -2,10 +2,12 @@ package com.example.demo.service;
 
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class SQSService {
 
@@ -19,7 +21,7 @@ public class SQSService {
     }
 
     public void sendMessage(String message) {
-        System.out.println("Message is " + message);
+        log.debug("Message is {}", message);
         SendMessageRequest request =
             new SendMessageRequest(sqs.getQueueUrl(queueName).getQueueUrl(), message);
         sqs.sendMessage(request);
